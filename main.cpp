34ac2bb5f05363104ac21f1b3c8d526e4fa82d3d
Eshaal Nazir <eshaalnazir05@gmail.com>
2026-06-21 15:33:11 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,73 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main()
 {
+    const int SIZE = 100;
+
+    int ids[SIZE];
+    string names[SIZE];
+    int count = 0;
+
     int choice;
 
-    cout << "===== UNIVERSITY ERP SYSTEM =====" << endl;
-    cout << "1. Add Student" << endl;
-    cout << "2. View Students" << endl;
-    cout << "3. Exit" << endl;
-
-    cout << "\nEnter Choice: ";
-    cin >> choice;
-
-    if(choice == 1)
+    while(true)
     {
-        int id;
-        string name;
+        cout << "\n===== UNIVERSITY ERP SYSTEM =====" << endl;
+        cout << "1. Add Student" << endl;
+        cout << "2. View Students" << endl;
+        cout << "3. Exit" << endl;
 
-        cout << "\nEnter Student ID: ";
-        cin >> id;
+        cout << "\nEnter Choice: ";
+        cin >> choice;
 
-        cin.ignore();
+        if(choice == 1)
+        {
+            if(count < SIZE)
+            {
+                cout << "\nEnter Student ID: ";
+                cin >> ids[count];
 
-        cout << "Enter Student Name: ";
-        getline(cin, name);
+                cin.ignore();
 
-        cout << "\nStudent Added Successfully!" << endl;
-        cout << "ID: " << id << endl;
-        cout << "Name: " << name << endl;
-    }
-    else if(choice == 2)
-    {
-        cout << "\nNo students available yet." << endl;
-    }
-    else if(choice == 3)
-    {
-        cout << "\nExiting..." << endl;
-    }
-    else
-    {
-        cout << "\nInvalid Choice!" << endl;
+                cout << "Enter Student Name: ";
+                getline(cin, names[count]);
+
+                count++;
+
+                cout << "\nStudent Added Successfully!" << endl;
+            }
+            else
+            {
+                cout << "\nStorage Full!" << endl;
+            }
+        }
+        else if(choice == 2)
+        {
+            if(count == 0)
+            {
+                cout << "\nNo students found!" << endl;
+            }
+            else
+            {
+                cout << "\n===== STUDENT LIST =====" << endl;
+
+                for(int i = 0; i < count; i++)
+                {
+                    cout << "ID: " << ids[i]
+                         << " | Name: " << names[i] << endl;
+                }
+            }
+        }
+        else if(choice == 3)
+        {
+            cout << "\nExiting..." << endl;
+            break;
+        }
+        else
+        {
+            cout << "\nInvalid Choice!" << endl;
+        }
     }
 
     return 0;
