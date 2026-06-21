6b56979eec4ff7d098503977c6abe552b784e836
Eshaal Nazir <eshaalnazir05@gmail.com>
2026-06-21 15:22:39 +0500

    Add files via upload
    Added basic student management

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
@@ -6,13 +7,42 @@ int main()
     int choice;
 
     cout << "===== UNIVERSITY ERP SYSTEM =====" << endl;
-    cout << "1. Student Management" << endl;
-    cout << "2. Teacher Management" << endl;
-    cout << "3. Course Management" << endl;
-    cout << "4. Exit" << endl;
+    cout << "1. Add Student" << endl;
+    cout << "2. View Students" << endl;
+    cout << "3. Exit" << endl;
 
     cout << "\nEnter Choice: ";
     cin >> choice;
+
+    if(choice == 1)
+    {
+        int id;
+        string name;
+
+        cout << "\nEnter Student ID: ";
+        cin >> id;
+
+        cin.ignore();
+
+        cout << "Enter Student Name: ";
+        getline(cin, name);
+
+        cout << "\nStudent Added Successfully!" << endl;
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+    }
+    else if(choice == 2)
+    {
+        cout << "\nNo students available yet." << endl;
+    }
+    else if(choice == 3)
+    {
+        cout << "\nExiting..." << endl;
+    }
+    else
+    {
+        cout << "\nInvalid Choice!" << endl;
+    }
 
     return 0;
 }
